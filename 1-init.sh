5452c43107a1b99d8529834804ed8c7824bbcb5e
HernaldoMeneses <140676491+HernaldoMeneses@users.noreply.github.com>
2024-03-07 12:30:20 -0300

    uptdate

--- 1-init.sh
+++ 1-init.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
-https://docs.docker.com/get-started/overview/
+#unbunto
+#https://docs.docker.com/get-started/overview/
+
+#install
+#basic
+#sudo apt install docker.io
+#basic + adm conteiners
+sudo apt install docker.io docker-compose
+
+#obtendo imagens populares do dockerhub
+#imagens servem para criar conteiners distintos
+sudo su
+#docker pull wordpress
+docker pull ubuntu
+#pesquise imagens polulares em:
+#https://hub.docker.com/search
+
+#Verificando as imagens
+docker images
